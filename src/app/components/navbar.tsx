b113cac41a66aebe60f2b0b52a4d46fd8185d7e4
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-07-07 11:41:18 +0700

    refactor: Improve navbar component for mobile responsiveness and usability

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,35 +3,86 @@
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import {useState} from "react";
-import {Icon} from "@iconify-icon/react";
+import {useEffect, useState} from "react";
+import { Icon } from "@iconify-icon/react";
 
 export default function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  return (
-    <nav className="flex justify-between items-center py-1 px-12 backdrop-blur-lg w-full sticky top-0 z-10">
-      <div>
-          <Image src="/brand-outline.svg" alt="Logo" width={120} height={120} />
-      </div>
-        <ul className="flex items-center gap-10">
-            <Link href="/">Beranda</Link>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-            <li className="relative">
-                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="dropbtn flex items-center">
-                    Paket Harga
-                    <Icon icon="mingcute:down-line" />
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleScroll = () => {
+        if (isMenuOpen) {
+            setIsMenuOpen(false);
+        }
+        if (isDropdownOpen) {
+            setIsDropdownOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [isMenuOpen, isDropdownOpen]);
+
+    return (
+        <nav className="flex justify-between items-center py-1 px-4 md:px-12 backdrop-blur-lg w-full sticky top-0 z-10 bg-gray-100 bg-opacity-50">
+            <div className="flex items-center pb-1">
+                <Image src="/brand-outline.svg" alt="Logo" width={100} height={100} />
+            </div>
+            <div className="md:hidden">
+                <button onClick={toggleMenu} className="text-black">
+                    <Icon icon={isMenuOpen ? "mingcute:close-line" : "mingcute:menu-line"} className="text-2xl" />
                 </button>
-                {isDropdownOpen && (
-                    <div className={`${styles.dropdownContent} ${isDropdownOpen ? styles.show : ''}`}>
-                        <Link href="/dropdown1">Dropdown 1</Link>
-                        <Link href="/dropdown2">Dropdown 2</Link>
-                    </div>
-                )}
-            </li>
-            <Link href="#testimoni">Testimoni</Link>
-            <Link href="#TentangKami">Tentang Kami</Link>
-            <Link href="#HubungiKami">Hubungi Kami</Link>
-        </ul>
-    </nav>
-  );
+            </div>
+            <ul className={`md:flex items-center gap-10 ${isMenuOpen ? 'block' : 'hidden'} md:block absolute text-center md:static top-[67px] left-0 w-full md:w-auto  bg-gray-200 md:bg-opacity-0 md:backdrop-blur-0 md:flex-row flex-col md:items-center transition-transform transform md:transform-none rounded-b-2xl`}>
+                <li className="py-4">
+                    <Link href="/" className="hover:text-secondary font-medium text-black">Beranda</Link>
+                </li>
+                <li className="py-4">
+                    <Link href="#testimoni" className="hover:text-secondary font-medium text-black">Testimoni</Link>
+                </li>
+                <li className="py-4">
+                    <Link href="#TentangKami" className="hover:text-secondary font-medium text-black">Tentang
+                        Kami</Link>
+                </li>
+                <li className="py-4">
+                    <Link href="#HubungiKami" className="hover:text-secondary font-medium text-black">Hubungi
+                        Kami</Link>
+                </li>
+                <li className="relative py-4">
+                    <button onClick={toggleDropdown}
+                            className="text-center mx-auto dropbtn flex items-center hover:text-secondary font-medium text-black">
+                        Paket Harga
+                        {isDropdownOpen ? (
+                            <Icon icon="mingcute:up-line"/> // Icon when dropdown is open
+                        ) : (
+                            <Icon icon="mingcute:down-line"/> // Default icon
+                        )}
+                    </button>
+                    {isDropdownOpen && (
+                        <div className={
+                            `${styles.dropdownContent} ${isDropdownOpen ? styles.show : ''}
+                            bg-gray-200 rounded-b-2xl
+                            `}>
+                            <Link href="/paket-dahua"
+                                  className="hover:text-secondary font-medium text-black">Dahua</Link>
+                            <Link href="/paket-hikvision"
+                                  className="hover:text-secondary font-medium text-black">Hikvision</Link>
+                            <Link href="/paket-imou" className="hover:text-secondary font-medium text-black">Imou</Link>
+                        </div>
+                    )}
+                </li>
+            </ul>
+        </nav>
+    );
 }

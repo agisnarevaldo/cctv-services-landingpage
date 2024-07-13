908eee02ad685da91ed52e3f9f053211d1d8ca47
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-07-13 23:16:28 +0700

    feat: Add Kontak page component for displaying contact information

---
+++ src/app/kontak/page.tsx
@@ -0,0 +1,13 @@
+import Contact from "@/app/components/contact";
+import Footer from "@/app/components/footer";
+import FloatingChatButton from "@/app/components/floatingChatButton";
+
+export default function Page () {
+    return (
+        <main className="min-h-screen">
+            <FloatingChatButton bottom={"bottom-4"} />
+            <Contact />
+            <Footer />
+        </main>
+    )
+}

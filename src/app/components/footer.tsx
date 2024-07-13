1e1cf16613c3cb750ea0adcd512643650cdd8950
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-07-13 23:19:54 +0700

    refactor: Update footer copyright text to display "Priangan CCTV"

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer () {
     const currentYear = new Date().getFullYear();
     return (
         <footer className="bg-bg bg-opacity-50 text-[12px] text-center py-2 text-black">
-            <p className="text-black">Copyright&copy; {currentYear} Wandi CCTV. All Rights Reserved.</p>
+            <p className="text-black">Copyright&copy; {currentYear} Priangan CCTV. All Rights Reserved.</p>
             {/*<p className="font-light">*/}
             {/*    Developed by{" "}*/}
             {/*    <Link className="underline" href="/https://github.com/agisnarevaldo">*/}
